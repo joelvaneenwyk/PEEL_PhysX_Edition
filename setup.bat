e12cff86b8d458c2041522995522de51dd11e37b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-03-16 09:48:37 -0700

    Add PhysX project generation to setup script
    (cherry picked from commit 5c2447268cc9ab96b6a7661b88db21d30c02fa3c)

--- setup.bat
+++ setup.bat
@@ -4,3 +4,5 @@ setlocal EnableDelayedExpansion
 
 cd /d "%~dp0\PEEL_Externals\JoltPhysics\Build"
 call "%~dp0\PEEL_Externals\JoltPhysics\Build\cmake_vs2022_cl.bat"
+
+call "%~dp0\PEEL_Externals\PhysX4\physx\generate_projects.bat" "vc17win64"
