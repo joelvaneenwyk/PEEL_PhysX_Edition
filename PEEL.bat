4acd50b0eb50e7bf6827cf10a8a707a5a01f8b53
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-08-18 19:23:04 -0700

    Misc configuration updates

--- PEEL.bat
+++ PEEL.bat
@@ -5,5 +5,5 @@ setlocal EnableDelayedExpansion
 call "%~dp0Bin\Build_PEEL.bat"
 if errorlevel 1 exit /b
 
-call "%~dp0Bin\Start_PEEL.bat" -p "PINT_PhysX4.dll" %*
+call "%~dp0Bin\Start_PEEL.bat" %*
 exit /b
